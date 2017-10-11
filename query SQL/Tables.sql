67d1fd78946d473d165ce237a0213330e64f07df
Eliomar-Rodriguez <antonio16--2012@hotmail.com>
2017-10-11 14:47:49 -0600

    He logrado reparar algunas de los errores que me daban, faltan 3 claves foraneas que me dan error

--- query SQL/Tables.sql
+++ query SQL/Tables.sql
@@ -1,7 +1,7 @@
 /*
 Tabla dimensiones, contiene las dimensiones las cuales son parte de los componentes
 */
-USE [Proyecto bases];
+USE ProyectoComunidad;
 GO
 CREATE TABLE Dimensiones(
 	ID			INT	IDENTITY(1,1)	NOT NULL,
@@ -13,7 +13,7 @@ GO
 /*
 Tabla componentes, contiene los componentes que son parte de un criterio a la hora de su evaluación
 */
-USE [Proyecto bases];
+USE ProyectoComunidad;
 GO
 CREATE TABLE Componentes(
 	ID				INT	IDENTITY(1,1)	NOT NULL,
@@ -29,7 +29,7 @@ GO
 /*
 Tabla CYE, contiene los criterios y estandares propuestos/inpuestos por SINAES
 */
-USE [Proyecto bases];
+USE ProyectoComunidad;
 GO
 CREATE TABLE CYE(
 	ID				INT	IDENTITY(1,1)	NOT NULL,
@@ -45,7 +45,7 @@ GO
 /*
 Tabla sedes, contiene todas las sedes del Tecnológico
 */
-USE [Proyecto bases];
+USE ProyectoComunidad;
 GO
 CREATE TABLE Sedes(
 	ID		INT	IDENTITY(1,1)		NOT NULL,
@@ -57,7 +57,7 @@ GO
 /*
 Tabla carreras, contiene todas las carreras del TEC en general
 */
-USE [Proyecto bases];
+USE ProyectoComunidad;
 GO
 CREATE TABLE Carreras(
 	ID			INT	IDENTITY(1,1)	NOT NULL,
@@ -74,7 +74,7 @@ GO
 Tabla autoevaluaciones anuales, dichas evaluaciones se relacionan a una persona quien es el encargado principal 
 de dicha evaluación
 */
-USE [Proyecto bases];
+USE ProyectoComunidad;
 GO
 CREATE TABLE AutoevaluacionesAnuales(
 	ID				INT	IDENTITY(1,1)		 NOT NULL,
@@ -85,31 +85,9 @@ CREATE TABLE AutoevaluacionesAnuales(
 GO
 
 /*
-Tabla Auto_CYEA, tabla que sale producto de la normalización entre las tablas AutoevaluacionesAnuales y CYEA y contiene 
-cada todos los criterios que se asocian a un usuario administrador encargado de la evaluación de una carrera
-*/
-
-
--->>>>>>>>>>>>> revisar que esten bien estas claves foraneas
-
-
-USE [Proyecto bases];
-GO
-CREATE TABLE Auto_CYEA(
-	ID_Autoeval		INT,
-	ID_CYEA			INT,
-	CONSTRAINT FK_Auto_CYEA_ID_Autoeval	FOREIGN KEY (ID_Autoeval) 
-		REFERENCES	dbo.AutoevaluacionesAnuales (ID) ON DELETE CASCADE ON UPDATE CASCADE,
-
-	--CONSTRAINT FK_Auto_CYEA_ID_CYEA	FOREIGN KEY (ID_CYEA) 
-		--REFERENCES	dbo.CYEA (ID) ON DELETE CASCADE ON UPDATE CASCADE
-);
-GO
-
-/*
 Tabla NivelesIAE, contiene los niveles IAE
 */
-USE [Proyecto bases];
+USE ProyectoComunidad;
 GO
 CREATE TABLE NivelesIAE(
 	ID			INT	IDENTITY(1,1)			NOT NULL,
@@ -121,7 +99,7 @@ GO
 /*
 Tabla Valoraciones, contiene las valoraciones las cuales serán en escala de malo hasta excelente
 */
-USE [Proyecto bases];
+USE ProyectoComunidad;
 GO
 CREATE TABLE Valoraciones(
 	ID			INT	IDENTITY(1,1)			NOT NULL,
@@ -133,7 +111,7 @@ GO
 /*
 Tabla CumplimientosNominales, contiene fechas en las que se deben presentar avances o tener listo algún criterio
 */
-USE [Proyecto bases];
+USE ProyectoComunidad;
 GO
 CREATE TABLE CumplimientosNominales(
 	ID					INT	IDENTITY(1,1)	NOT NULL,
@@ -143,26 +121,9 @@ CREATE TABLE CumplimientosNominales(
 GO
 
 /*
-Tabla CumplimientosNominales_CYEA, tabla producida por la normalización de la relación entre las tablas CYEA y
-CumplimientosNominales
-*/
-USE [Proyecto bases];
-GO
-CREATE TABLE CumplimientosNominales_CYEA(
-	ID_CumplimNominal	INT,
-	ID_CYEA				INT,
-	--CONSTRAINT FK_CumplimientosNominales_CYEA_ID_CYEA	FOREIGN KEY (ID_CYEA) 
-		--REFERENCES	dbo.CYEA (ID) ON DELETE CASCADE ON UPDATE CASCADE,
-	
-	CONSTRAINT FK_CumplimientosNominales_CYEA_ID_CumplimNominal	FOREIGN KEY (ID_CumplimNominal) 
-		REFERENCES	dbo.CumplimientosNominales (ID) ON DELETE CASCADE ON UPDATE CASCADE
-);
-GO
-
-/*
 Tabla Responsables, contiene las personas que son responsables de que un criterio se cumpla bajo los requisitos de SINAES
 */
-USE [Proyecto bases];
+USE ProyectoComunidad;
 GO
 CREATE TABLE Responsables(
 	ID			INT	IDENTITY(1,1)	NOT NULL,
@@ -172,26 +133,9 @@ CREATE TABLE Responsables(
 GO
 
 /*
-Tabla CYEA_Responsables, tabla producida por la normalización de la relación entre las tablas CYEA y Responsables
-*/
-USE [Proyecto bases];
-GO
-CREATE TABLE CYEA_Responsables(
-	ID_CYEA			INT,
-	ID_Responsable	INT,
-	TipoResponsabilidad VARCHAR(250),
-	--CONSTRAINT FK_CYEA_Responsables_ID_CYEA	FOREIGN KEY (ID_CYEA) 
-		--REFERENCES	dbo.CYEA (ID) ON DELETE CASCADE ON UPDATE CASCADE,
-
-	CONSTRAINT FK_CYEA_Responsables_ID_Responsable	FOREIGN KEY (ID_Responsable) 
-		REFERENCES	dbo.Responsables (ID) ON DELETE CASCADE ON UPDATE CASCADE
-);
-GO
-
-/*
 Tabla Evidencias, contiene las evidencias que prueban el correcto cumplimiento de un criterio ajustado
 */
-USE [Proyecto bases];
+USE ProyectoComunidad;
 GO
 CREATE TABLE Evidencias(
 	ID				INT	IDENTITY(1,1)	NOT NULL,
@@ -202,40 +146,24 @@ CREATE TABLE Evidencias(
 GO
 
 /*
-Tabla CYEA_Evidencias, tabla producida por la normalización de la relación entre las tablas CYEA y Evidencias
-*/
-USE [Proyecto bases];
-GO
-CREATE TABLE CYEA_Evidencias(
-	ID_CYEA				INT,
-	ID_Evidencia		INT,
-	--CONSTRAINT FK_CYEA_Evidencias_ID_CYEA	FOREIGN KEY (ID_CYEA) 
-		--REFERENCES	dbo.CYEA (ID) ON DELETE CASCADE ON UPDATE CASCADE,
-
-	CONSTRAINT FK_CYEA_Evidencias_ID_Evidencia	FOREIGN KEY (ID_Evidencia) 
-		REFERENCES	dbo.Evidencias (ID) ON DELETE CASCADE ON UPDATE CASCADE
-);
-GO
-
-/*
 Tabla CYEA, tabla principal que contiene todos los CYE ya ajustados y listos para la autoevaluación
 */
-USE [Proyecto bases];
+USE ProyectoComunidad;
 GO
 CREATE TABLE CYEA(
-	ID					INT IDENTITY(1,1)	NOT NULL,
-	ID_CYE_General		INT,
-	ID_Sede				INT,
-	ID_Carrera			INT,
-	ID_Evidencia		INT,
-	ID_Valoracion		INT,
-	ID_CumplimientoNominal INT,
-	ID_NivelIAE			INT,
-	CriterioAjustado	VARCHAR(350),
-	FLOC				DATE,
-	FLA					DATE,
-	IncorporadoIAE		BIT,
-	Observaciones		VARCHAR(500),
+	ID						INT IDENTITY(1,1)	NOT NULL,
+	ID_CYE_General			INT,
+	ID_Sede					INT,
+	ID_Carrera				INT,
+	ID_Evidencia			INT,
+	ID_Valoracion			INT,
+	ID_CumplimientoNominal	INT,
+	ID_NivelIAE				INT,
+	CriterioAjustado		VARCHAR(350)		NOT NULL,
+	FLOC					DATE				NOT NULL,
+	FLA						DATE				NOT NULL,
+	IncorporadoIAE			BIT					NOT NULL,
+	Observaciones			VARCHAR(500)		NOT NULL,
 
 	CONSTRAINT PK_CYEA_ID PRIMARY KEY CLUSTERED (ID),
 
@@ -244,9 +172,6 @@ CREATE TABLE CYEA(
 
 	CONSTRAINT FK_CYEA_ID_Sede	FOREIGN KEY (ID_Sede) 
 		REFERENCES	dbo.Sedes (ID) ON DELETE CASCADE ON UPDATE CASCADE,
-
-	--CONSTRAINT FK_CYEA_ID_Carrera FOREIGN KEY (ID_Carrera) 
-		--REFERENCES dbo.Carreras (ID) ON DELETE CASCADE ON UPDATE CASCADE,
 
 	CONSTRAINT FK_CYEA_ID_Evidencia	FOREIGN KEY (ID_Evidencia) 
 		REFERENCES	dbo.Evidencias (ID) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -259,5 +184,82 @@ CREATE TABLE CYEA(
 
 	CONSTRAINT FK_CYEA_ID_NivelIAE	FOREIGN KEY (ID_NivelIAE) 
 		REFERENCES	dbo.NivelesIAE (ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+GO
+
+/*
+Tabla CYEA_Responsables, tabla producida por la normalización de la relación entre las tablas CYEA y Responsables
+*/
+USE ProyectoComunidad;
+GO
+CREATE TABLE CYEA_Responsables(
+	ID_CYEA			INT,
+	ID_Responsable	INT,
+	TipoResponsabilidad VARCHAR(250),
+	CONSTRAINT FK_CYEA_Responsables_ID_CYEA	FOREIGN KEY (ID_CYEA) 
+		REFERENCES	dbo.CYEA (ID) ON DELETE CASCADE ON UPDATE CASCADE,
+
+	CONSTRAINT FK_CYEA_Responsables_ID_Responsable	FOREIGN KEY (ID_Responsable) 
+		REFERENCES	dbo.Responsables (ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+GO
+
+/*
+Tabla Auto_CYEA, tabla que sale producto de la normalización entre las tablas AutoevaluacionesAnuales y CYEA y contiene 
+cada todos los criterios que se asocian a un usuario administrador encargado de la evaluación de una carrera
+*/
+USE ProyectoComunidad;
+GO
+CREATE TABLE Auto_CYEA(
+	ID_Autoeval		INT,
+	ID_CYEA			INT,
+	CONSTRAINT FK_Auto_CYEA_ID_Autoeval	FOREIGN KEY (ID_Autoeval) 
+		REFERENCES	dbo.AutoevaluacionesAnuales (ID) ON DELETE CASCADE ON UPDATE CASCADE,
+
+	CONSTRAINT FK_Auto_CYEA_ID_CYEA	FOREIGN KEY (ID_CYEA) 
+		REFERENCES	dbo.CYEA (ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+GO
+-----------------------------------------------------------------------------
+--						ERRORES CON CLAVES FORANEAS						   --
+-----------------------------------------------------------------------------
+
+-- TRATANDO DE AGREGAR ESA CLAVE FORANEA DE MANERA EXTERNA
+-- esta clave foranea es de la tabla CYEA solo que no me funciona y estoy tratando de agregarla de manera externa
+-- si funcion si pongo ON DELETE NO ACTION ON UPDATE NO ACTION pero esa no es la idea
+ALTER TABLE CYEA ADD CONSTRAINT FK_CYEA_ID_Carrera FOREIGN KEY (ID_Carrera) 
+		REFERENCES dbo.Carreras (ID) ON DELETE CASCADE ON UPDATE NO ACTION
+GO
+
+/*
+Tabla CumplimientosNominales_CYEA, tabla producida por la normalización de la relación entre las tablas CYEA y
+CumplimientosNominales
+*/
+USE ProyectoComunidad;
+GO
+CREATE TABLE CumplimientosNominales_CYEA(
+	ID_CumplimNominal	INT,
+	ID_CYEA				INT,
+	CONSTRAINT FK_CumplimientosNominales_CYEA_ID_CYEA	FOREIGN KEY (ID_CYEA) 
+		REFERENCES	dbo.CYEA (ID) ON DELETE CASCADE ON UPDATE CASCADE,
+	
+	CONSTRAINT FK_CumplimientosNominales_CYEA_ID_CumplimNominal	FOREIGN KEY (ID_CumplimNominal) 
+		REFERENCES	dbo.CumplimientosNominales (ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+GO
+
+/*
+Tabla CYEA_Evidencias, tabla producida por la normalización de la relación entre las tablas CYEA y Evidencias
+*/
+USE ProyectoComunidad;
+GO
+CREATE TABLE CYEA_Evidencias(
+	ID_CYEA				INT,
+	ID_Evidencia		INT,
+	CONSTRAINT FK_CYEA_Evidencias_ID_CYEA	FOREIGN KEY (ID_CYEA) 
+		REFERENCES	dbo.CYEA (ID) ON DELETE CASCADE ON UPDATE CASCADE,
+
+	CONSTRAINT FK_CYEA_Evidencias_ID_Evidencia	FOREIGN KEY (ID_Evidencia) 
+		REFERENCES	dbo.Evidencias (ID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 GO
